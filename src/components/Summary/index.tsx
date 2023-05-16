178224fdefbf83c01f5843981f9d92640408cb3a
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-05-16 09:18:55 -0300

    creating summary component adding summaryCard to component styling

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,33 +1,33 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
-import { SummaryContainer } from "./styles"
+import { SummaryCard, SummaryContainer } from "./styles"
 
 export function Summary() {
     return (
         <SummaryContainer>
-            <div>
+            <SummaryCard>
                 <header>
                     <span>Entrada</span>
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
 
                 <strong>R$ 100.00,00</strong>
-            </div>
-            <div>
+            </SummaryCard>
+            <SummaryCard>
                 <header>
                     <span>Saída</span>
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
 
                 <strong>R$ 100.00,00</strong>
-            </div>
-            <div>
+            </SummaryCard>
+            <SummaryCard>
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
                 <strong>R$ 100.00,00</strong>
-            </div>
+            </SummaryCard>
         </SummaryContainer>
     );
 }
