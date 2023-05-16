79f2596e0fa83b3ba3153ef18c59c81336366817
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-05-16 09:34:26 -0300

    creating table component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,11 +1,28 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 25.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>venda</td>
+                            <td>16/05/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
