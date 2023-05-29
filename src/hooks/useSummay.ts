d0efcd2a69668e5735379b0bf8917169a08da03d
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-05-29 12:29:23 -0300

    applying the useMemo hook

--- src/hooks/useSummay.ts
+++ src/hooks/useSummay.ts
@@ -1,31 +1,34 @@
 import { useContextSelector } from 'use-context-selector';
-import { TransactionsContext } from "../contexts/TransactionsContext";
+import { TransactionsContext } from '../contexts/TransactionsContext';
+import { useMemo } from 'react';
 
 export function useSummary() {
     const transactions = useContextSelector(TransactionsContext, (context) => {
         return context.transactions
     });
 
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            switch (transaction.type) {
-                case 'income':
-                    acc.income += transaction.price;
-                    acc.total += transaction.price;
-                    break;
-                default:
-                    acc.outcome += transaction.price;
-                    acc.total -= transaction.price;
+    const summary = useMemo(() => {
+        return transactions.reduce(
+            (acc, transaction) => {
+                switch (transaction.type) {
+                    case 'income':
+                        acc.income += transaction.price;
+                        acc.total += transaction.price;
+                        break;
+                    default:
+                        acc.outcome += transaction.price;
+                        acc.total -= transaction.price;
+                }
+                
+                return acc;
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0
             }
-            
-            return acc;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    )
+        )
+    }, [transactions])
 
     return summary;
 }
